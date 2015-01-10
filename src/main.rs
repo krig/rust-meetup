01e9d4674481d377c265c3051a426b72846fadc1
Kristoffer Grönlund <krig@koru.se>
2015-01-10 21:22:08 +0100

    An attempt to update this to 1.0 alpha. Failed. I think I am done with rust.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,43 @@
-#![feature(if_let)]
+use std::ops::Add;
 use std::io;
+use std::num;
 
-fn roman(num: uint) -> String {
-    fn roman2(num: uint, mul: uint, base: &'static str, mid: &'static str) -> String {
+fn roman(num: usize) -> String {
+    fn roman2(num: usize, mul: usize, base: &'static str, mid: &'static str) -> String {
         match num {
-            1 => base.to_string(),
+            1 => String::from_str(base),
             2...3 => roman(1 * mul) + roman((num - 1) * mul),
             4 => roman(1 * mul) + roman(5 * mul),
-            5 => mid.to_string(),
+            5 => String::from_str(mid),
             6...8 => roman((num - 5) * mul) + roman(5 * mul),
             9 => roman(1 * mul) + roman(10 * mul),
-            _ => "".to_string()
+            _ => String::from_str("")
         }
     }
 
-    roman2(num / 1000, 1000, "M", "MMMMM") +
-        roman2((num % 1000) / 100, 100, "C", "D") +
-        roman2((num % 100) / 10, 10, "X", "L") +
-        roman2(num % 10, 1, "I", "V")
+    let mut s = roman2(num / 1000, 1000, "M", "MMMMM");
+    s.push_str(roman2((num % 1000) / 100, 100, "C", "D").as_slice());
+    s.push_str(roman2((num % 100) / 10, 10, "X", "L").as_slice());
+    s.push_str(roman2(num % 10, 1, "I", "V").as_slice());
+    s
 }
 
 #[test]
 fn test_roman() {
-    assert!(roman(1954) == "MCMLIV".to_string())
-    assert!(roman(1990) == "MCMXC".to_string())
-    assert!(roman(2014) == "MMXIV".to_string())
+    assert!(roman(1954).as_slice() == "MCMLIV")
+    //std::assert!(roman(1990).as_slice() == "MCMXC")
+    //std::assert!(roman(2014).as_slice() == "MMXIV")
 }
 
 fn main() {
     loop {
         println!("Enter a number: ");
         let input = io::stdin().read_line().ok().expect("Failed to read line");
-        if let Some(num) = from_str(input.as_slice().trim()) {
-            println!("{}", roman(num))
+        let input_num: Option<usize>  = input.parse();
+        if let Some(num) = input_num {
+           println!("{}", roman(num))
         } else {
-            return
+          return
         }
     }
 }
