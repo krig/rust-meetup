0a539d2ab57e004c9d08d6b5756ac98790b59995
Kristoffer Grönlund <krig@koru.se>
2015-01-11 02:45:58 +0100

    Alright, this compiles.
    Apparently, the String + String operator is not implemented, only String
    + &str is.
    
    Also getting this lovely warning...
    
    warning: use of unstable item: Instead of taking this bound generically,
    	this trait will be replaced with one of slicing syntax, deref
    	coercions, or a more generic conversion trait, #[warn(unstable)]
    	on by default
    
    Competing with the C++ compilers for most obtuse output, I see.

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,45 @@
-use std::ops::Add;
 use std::io;
-use std::num;
 
-fn roman(num: usize) -> String {
-    fn roman2(num: usize, mul: usize, base: &'static str, mid: &'static str) -> String {
+fn roman(num: i32) -> String {
+    fn roman2(num: i32, mul: i32, base: &'static str, mid: &'static str) -> String {
         match num {
             1 => String::from_str(base),
-            2...3 => roman(1 * mul) + roman((num - 1) * mul),
-            4 => roman(1 * mul) + roman(5 * mul),
+            2...3 => roman(1 * mul) + roman((num - 1) * mul).as_slice(),
+            4 => roman(1 * mul) + roman(5 * mul).as_slice(),
             5 => String::from_str(mid),
-            6...8 => roman((num - 5) * mul) + roman(5 * mul),
-            9 => roman(1 * mul) + roman(10 * mul),
+            6...8 => roman((num - 5) * mul) + roman(5 * mul).as_slice(),
+            9 => roman(1 * mul) + roman(10 * mul).as_slice(),
             _ => String::from_str("")
         }
     }
 
-    let mut s = roman2(num / 1000, 1000, "M", "MMMMM");
-    s.push_str(roman2((num % 1000) / 100, 100, "C", "D").as_slice());
-    s.push_str(roman2((num % 100) / 10, 10, "X", "L").as_slice());
-    s.push_str(roman2(num % 10, 1, "I", "V").as_slice());
-    s
+    roman2(num / 1000, 1000, "M", "MMMMM") +
+        roman2((num % 1000) / 100, 100, "C", "D").as_slice() +
+        roman2((num % 100) / 10, 10, "X", "L").as_slice() +
+        roman2(num % 10, 1, "I", "V").as_slice()
 }
 
 #[test]
 fn test_roman() {
-    assert!(roman(1954).as_slice() == "MCMLIV")
-    //std::assert!(roman(1990).as_slice() == "MCMXC")
-    //std::assert!(roman(2014).as_slice() == "MMXIV")
+    assert!(roman(1954).as_slice() == "MCMLIV");
+    assert!(roman(1990).as_slice() == "MCMXC");
+    assert!(roman(2014).as_slice() == "MMXIV");
 }
 
 fn main() {
     loop {
         println!("Enter a number: ");
-        let input = io::stdin().read_line().ok().expect("Failed to read line");
-        let input_num: Option<usize>  = input.parse();
-        if let Some(num) = input_num {
-           println!("{}", roman(num))
+        let input: Option<i32>  = io::stdin()
+            .read_line()
+            .ok()
+            .expect("Failed to read line")
+            .trim()
+            .parse();
+        if let Some(num) = input {
+            println!("{}", roman(num))
         } else {
-          return
+            println!("Je suis Charlie");
+            return
         }
     }
 }
