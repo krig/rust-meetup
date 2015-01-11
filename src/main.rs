c36a4842966bf0848dd6fe1afc657e8cb1f40f30
Kristoffer Grönlund <krig@koru.se>
2015-01-11 02:58:08 +0100

    lol.

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,26 @@ fn roman(num: i32) -> String {
     fn roman2(num: i32, mul: i32, base: &'static str, mid: &'static str) -> String {
         match num {
             1 => String::from_str(base),
-            2...3 => roman(1 * mul) + roman((num - 1) * mul).as_slice(),
-            4 => roman(1 * mul) + roman(5 * mul).as_slice(),
+            2...3 => roman(1 * mul) + &roman((num - 1) * mul)[],
+            4 => roman(1 * mul) + &roman(5 * mul)[],
             5 => String::from_str(mid),
-            6...8 => roman((num - 5) * mul) + roman(5 * mul).as_slice(),
-            9 => roman(1 * mul) + roman(10 * mul).as_slice(),
+            6...8 => roman((num - 5) * mul) + &roman(5 * mul)[],
+            9 => roman(1 * mul) + &roman(10 * mul)[],
             _ => String::from_str("")
         }
     }
 
     roman2(num / 1000, 1000, "M", "MMMMM") +
-        roman2((num % 1000) / 100, 100, "C", "D").as_slice() +
-        roman2((num % 100) / 10, 10, "X", "L").as_slice() +
-        roman2(num % 10, 1, "I", "V").as_slice()
+        &roman2((num % 1000) / 100, 100, "C", "D")[] +
+        &roman2((num % 100) / 10, 10, "X", "L")[] +
+        &roman2(num % 10, 1, "I", "V")[]
 }
 
 #[test]
 fn test_roman() {
-    assert!(roman(1954).as_slice() == "MCMLIV");
-    assert!(roman(1990).as_slice() == "MCMXC");
-    assert!(roman(2014).as_slice() == "MMXIV");
+    assert!(&roman(1954)[] == "MCMLIV");
+    assert!(&roman(1990)[] == "MCMXC");
+    assert!(&roman(2014)[] == "MMXIV");
 }
 
 fn main() {
