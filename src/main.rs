159882614c097d71490a6a3ed4a628e863eb5623
Kristoffer Grönlund <krig@koru.se>
2015-05-06 16:44:20 +0200

    Finally figured out how to port this to 1.0.0-beta3

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,48 @@
 use std::io;
 
-fn roman(num: i32) -> String {
-    fn roman2(num: i32, mul: i32, base: &'static str, mid: &'static str) -> String {
+fn roman(num: u64) -> String {
+    fn roman2(num: u64, mul: u64, base: &'static str, mid: &'static str) -> String {
         match num {
-            1 => String::from_str(base),
-            2...3 => roman(1 * mul) + &roman((num - 1) * mul)[],
-            4 => roman(1 * mul) + &roman(5 * mul)[],
-            5 => String::from_str(mid),
-            6...8 => roman((num - 5) * mul) + &roman(5 * mul)[],
-            9 => roman(1 * mul) + &roman(10 * mul)[],
-            _ => String::from_str("")
+            1 => base.to_string(),
+            2...3 => format!("{}{}", roman(1 * mul), roman((num - 1) * mul)),
+            4 => format!("{}{}", roman(1 * mul), roman(5 * mul)),
+            5 => mid.to_string(),
+            6...8 => format!("{}{}", roman((num - 5) * mul), roman(5 * mul)),
+            9 => format!("{}{}", roman(1 * mul), roman(10 * mul)),
+            _ => "".to_string()
         }
     }
 
-    roman2(num / 1000, 1000, "M", "MMMMM") +
-        &roman2((num % 1000) / 100, 100, "C", "D")[] +
-        &roman2((num % 100) / 10, 10, "X", "L")[] +
-        &roman2(num % 10, 1, "I", "V")[]
+    format!("{}{}{}{}",
+            roman2(num / 1000, 1000, "M", "MMMMM"),
+            roman2((num % 1000) / 100, 100, "C", "D"),
+            roman2((num % 100) / 10, 10, "X", "L"),
+            roman2(num % 10, 1, "I", "V"))
+}
+
+macro_rules! assert_eq {
+    ($a: expr, $b: expr) => {
+        {
+            let a = $a;
+            let b = $b;
+            assert!(a == b);
+        }
+    }
 }
 
 #[test]
-fn test_roman() {
-    assert!(&roman(1954)[] == "MCMLIV");
-    assert!(&roman(1990)[] == "MCMXC");
-    assert!(&roman(2014)[] == "MMXIV");
+fn basic_conversion() {
+    assert_eq!(roman(1954), "MCMLIV".to_string());
+    assert_eq!(roman(1990), "MCMXC".to_string());
+    assert_eq!(roman(2014), "MMXIV".to_string());
 }
 
 fn main() {
     loop {
         println!("Enter a number: ");
-        let input: Option<i32>  = io::stdin()
-            .read_line()
-            .ok()
-            .expect("Failed to read line")
-            .trim()
-            .parse();
+        let mut buf = String::new();
+        io::stdin().read_line(&mut buf).ok().expect("failed to read line");
+        let input: Option<u64> = buf.trim().parse().ok();
         if let Some(num) = input {
             println!("{}", roman(num))
         } else {
